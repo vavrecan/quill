6e5d88761b7c422a680ece369acb9d7136b2eb8f
Marek Vavrecan <vavrecan@gmail.com>
2020-11-28 13:25:37 +0100

    support for title attribute in images

--- formats/image.js
+++ formats/image.js
@@ -1,7 +1,7 @@
 import { EmbedBlot } from 'parchment';
 import { sanitize } from './link';
 
-const ATTRIBUTES = ['alt', 'height', 'width'];
+const ATTRIBUTES = ['alt', 'height', 'width', 'title'];
 
 class Image extends EmbedBlot {
   static create(value) {
